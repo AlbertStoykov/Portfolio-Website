e45b18f4a3e5a1a24f66b24cb70c598591d8c7bd
AlbertStoykov <albert.stoykov@gmail.com>
2022-09-26 20:06:06 +0100

    Update page.js

--- js/page.js
+++ js/page.js
@@ -15,6 +15,12 @@ function topnav() {
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    } else {
+      entry.target.classList.remove("visible");
+    }
   });
 });
 const scrollElements = document.querySelectorAll(".scroll-animation");
+scrollElements.forEach((el) => observer.observe(el));
