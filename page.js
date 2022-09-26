5f0c89c6c191e8836a24c292ffd85bff806f1653
AlbertStoykov <albert.stoykov@gmail.com>
2022-09-26 20:56:46 +0100

    js not linking to html

---
+++ page.js
@@ -0,0 +1,26 @@
+// const navToggle = document.querySelector(".nav-toggle");
+// navToggle.addEventListener("click", () => {
+//   document.body.classList.toggle("nav-open");
+// });
+
+// function topnav() {
+//   var x = getElementById("topnav");
+//   if (x.className === "topnav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "topnav";
+//   }
+// }
+console.log(1);
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    } else {
+      entry.target.classList.remove("visible");
+    }
+  });
+});
+const scrollElements = document.querySelectorAll(".scroll-animation");
+scrollElements.forEach((el) => observer.observe(el));
